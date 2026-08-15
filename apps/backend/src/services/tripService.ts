7f1e07880f31dea094d6887b430d81ef169f60c2
crystalkode <crystal.shih1126@gmail.com>
2026-08-15 17:09:48 +0900

    feat: add updateTrips endpoint

--- apps/backend/src/services/tripService.ts
+++ apps/backend/src/services/tripService.ts
@@ -1,22 +1,39 @@
-import { prisma } from "../db/prisma"
+import { prisma } from "../db/prisma";
 import { handlePrismaError } from "../utils/prismaErrorHandler";
 import { AppError } from "../errors/AppError";
+import * as z from "zod";
+import {updateTripSchema} from "../schemas/tripSchema"
 
 type CreateTripInput = {
-  name: string
-  startDate?: Date
-  endDate?: Date
-  userId: string
-}
+  name: string;
+  startDate?: Date;
+  endDate?: Date;
+  userId: string;
+};
 
 export async function createTrip(data: CreateTripInput) {
   if (!data.name) throw new AppError("Trip name is required", 400);
-  
+
   return prisma.trip.create({
-    data
-  })
+    data,
+  });
+}
+
+type UpdateTripInput = z.infer<typeof updateTripSchema>;
+
+export async function updateTrip(tripId: string, input: UpdateTripInput) {
+  if (!tripId) throw new AppError("Trip Id is required", 400);
+
+  try {
+    return prisma.trip.update({
+      where: { id: tripId },
+      data: input,
+    });
+  } catch (error) {
+    throw handlePrismaError(error, `Trip ${tripId}`);
+  }
 }
 
 export async function getTrips() {
-  return prisma.trip.findMany()
+  return prisma.trip.findMany();
 }
